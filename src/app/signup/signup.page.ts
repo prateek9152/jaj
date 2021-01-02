bbb0a3b19190fb7d2edd2920137c41723a4beb1d
Prateek Yaadav <prateek.yadav@wdipl.com>
2021-01-02 15:46:15 +0530

    changessignup

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -56,10 +56,12 @@ export class SignupPage implements OnInit {
         Validators.pattern('[a-zA-Z0-9_.+-]+@(?:[a-zA-Z0-9-]+\.)\.[A-Za-z0-9._%+-]{2,}'),
         //  Validators.pattern('[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(gmail|wdipl|yahoo|live|outlook|hotmail|yopmail|gmx)\.[A-Za-z0-9._%+-]{2,}')
       ])),
-      mobile_number:new FormControl('',Validators.compose([
+      primary_mobile_number:new FormControl('',Validators.compose([
         Validators.required,Validators.minLength(10)
       ])),
-     
+      secondary_mobile_number:new FormControl('',Validators.compose([
+        Validators.required,Validators.minLength(10)
+      ])),
       password: new FormControl('',Validators.compose([
         Validators.required,
         Validators.maxLength(25),
@@ -85,10 +87,14 @@ export class SignupPage implements OnInit {
       { type: 'required', message: 'Email is required.' },
       { type: 'pattern', message: 'Enter a valid email' }
     ],
-    mobile_number:[
+    primary_mobile_number:[
       { type: 'required', message: 'Mobile Number is required.' },
       { type: 'minlength', message: 'Mobile Number must be at least 10 numbers.' },
       ],
+      secondary_mobile_number:[
+        { type: 'required', message: 'Mobile Number is required.' },
+        { type: 'minlength', message: 'Mobile Number must be at least 10 numbers.' },
+        ],
     password: [
       { type: 'required', message: 'Password is required.' },
       { type: 'minlength', message: 'Password must be at least 8 characters long.' },
@@ -130,13 +136,15 @@ export class SignupPage implements OnInit {
     formData.append("name",this.registerForm.get('name').value);
     formData.append("last_name",this.registerForm.get('last_name').value);
     formData.append("user_name",this.registerForm.get('user_name').value);
-    formData.append("mobile_number",this.registerForm.get('mobile_number').value);
+    formData.append("primary_mobile_number",this.registerForm.get('primary_mobile_number').value);
+
+    formData.append("secondary_mobile_number",this.registerForm.get('secondary_mobile_number').value);
     formData.append("email",this.registerForm.get('email').value);
     formData.append("password",this.registerForm.get('password').value);
     formData.append("c_password",this.registerForm.get('c_password').value);
 
     let headers = new HttpHeaders({"Accept": "application/json"});
-    this.http.post('https://jajoj.threethree.in/api/register',formData,{headers:headers}).subscribe(async (response:any)=> {
+    this.http.post('https://intoactive.wdipl.com/mychat/api/register',formData,{headers:headers}).subscribe(async (response:any)=> {
               this.router.navigate(['/menu/verify']);
     //         if(response.status==1){
     //           this.storage.set('userData', this.registerForm.value.email);
